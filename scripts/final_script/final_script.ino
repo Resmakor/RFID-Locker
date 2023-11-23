0c6ad11632fb67cc304c2826c3e56c3f75f96121
Bartłomiej Czajka <52079500+Resmakor@users.noreply.github.com>
2023-11-23 20:28:17 +0100

    Getting started with final script
    - LCD and RFID works now

---
+++ scripts/final_script/final_script.ino
@@ -0,0 +1,63 @@
+#include <SPI.h>
+#include <MFRC522.h>
+#include <LiquidCrystal.h> 
+#define RST_PIN 9         
+#define SS_PIN 10      
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+LiquidCrystal lcd(8, 7, 5, 4, 3, 2);  
+int Contrast = 75;
+String correct_UID = "C925E914";
+
+void setup() {
+	Serial.begin(9600);
+	while (!Serial);		
+	SPI.begin();	
+	mfrc522.PCD_Init();
+	delay(4);
+	mfrc522.PCD_DumpVersionToSerial();
+  analogWrite(6, Contrast);
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  lcd.print("ZAMKNIETE");
+  lcd.setCursor(0, 1);
+  lcd.print("ZBLIZ KARTE");
+  delay(4);
+}
+
+void loop() {
+	if ( ! mfrc522.PICC_IsNewCardPresent()) {
+		return;
+	}
+	if ( ! mfrc522.PICC_ReadCardSerial()) {
+		return;
+	}
+  String cardUID = "";
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+      cardUID += String(mfrc522.uid.uidByte[i], HEX);
+  }
+  cardUID.toUpperCase();
+  if (correct_UID == cardUID)
+  {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("OTWARTE");
+    delay(5000);
+    lcd.clear();
+  }
+  else
+  {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("NIEPOPRAWNA");
+    lcd.setCursor(0, 1);
+    lcd.print("KARTA");
+    delay(3000);
+    lcd.clear();
+  }
+  lcd.setCursor(0, 0);
+  lcd.print("ZAMKNIETE");
+  lcd.setCursor(0, 1);
+  lcd.print("ZBLIZ KARTE");
+  delay(4);
+}
